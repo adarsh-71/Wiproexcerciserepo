a8cc44e2f2900af6166a994e641af0f809affa1b
Adarsh Pandey <117114473+adarsh-71@users.noreply.github.com>
2025-09-02 05:24:54 +0530

    Add files via upload

---
+++ Capstone_Project/Backend/product/src/main/java/com/wipro/ecom/product/entity/Product.java
@@ -0,0 +1,27 @@
+package com.wipro.ecom.product.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "image_url") 
+    private String imageUrl;
+}
